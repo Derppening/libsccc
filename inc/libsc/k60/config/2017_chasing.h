0fdc9ca7bcd2c796d4fa52e77a5e6529009c7cb6
David Mak <david.18.19.21@gmail.com>
2017-03-30 14:21:44 +0800

    Update 2017_CHASING configuration

--- inc/libsc/k60/config/2017_chasing.h
+++ inc/libsc/k60/config/2017_chasing.h
@@ -4,11 +4,12 @@
 #define LIBSC_USE_BUTTON 2
 #define LIBSC_USE_BUZZER 1
 #define LIBSC_USE_ENCODER 2
+// #define LIBSC_USE_INFRA_RED_SENSOR 2
 #define LIBSC_USE_JOYSTICK 1
 #define LIBSC_USE_LCD 1
 #define LIBSC_USE_LED 4
 #define LIBSC_USE_MOTOR 2
-// #define LIBSC_USE_LINEAR_CCD 2
+// #define LIBSC_USE_MPU6050 1
 #define LIBSC_USE_OV7725 1
 #define LIBSC_USE_SERVO 1
 #define LIBSC_USE_UART 1
@@ -19,7 +20,6 @@
 #define LIBSC_BUTTON1 libbase::k60::Pin::Name::kPtc12
 
 #define LIBSC_BUZZER0 libbase::k60::Pin::Name::kPta8
-//#define LIBSC_BUZZER_ACIVE_LEVEL 0
 
 #define LIBSC_ENCODER0_QDA libbase::k60::Pin::Name::kPta10
 #define LIBSC_ENCODER0_QDB libbase::k60::Pin::Name::kPta11
@@ -43,11 +43,6 @@
 #define LIBSC_LED2 libbase::k60::Pin::Name::kPte10
 #define LIBSC_LED3 libbase::k60::Pin::Name::kPte9
 
-//#define LIBSC_MOTOR0_PWM libbase::k60::Pin::Name::kPtd0
-//#define LIBSC_MOTOR0_DIR libbase::k60::Pin::Name::kPtd1
-//#define LIBSC_MOTOR1_PWM libbase::k60::Pin::Name::kPtd2
-//#define LIBSC_MOTOR1_DIR libbase::k60::Pin::Name::kPtd3
-
 #define LIBSC_MOTOR0_PWMA libbase::k60::Pin::Name::kPtd0
 #define LIBSC_MOTOR0_PWMB libbase::k60::Pin::Name::kPtd1
 #define LIBSC_MOTOR0_DEADTIME 1000
@@ -56,13 +51,6 @@
 #define LIBSC_MOTOR1_PWMB libbase::k60::Pin::Name::kPtd3
 #define LIBSC_MOTOR1_DEADTIME 1000
 #define LIBSC_ALTERNATE_MOTOR_CW_PWM 1
-
-// #define LIBSC_LINEAR_CCD0_SI libbase::k60::Pin::Name::kPtb7
-// #define LIBSC_LINEAR_CCD0_CLK libbase::k60::Pin::Name::kPtb10
-// #define LIBSC_LINEAR_CCD0_AD libbase::k60::Pin::Name::kPtb4
-// #define LIBSC_LINEAR_CCD1_SI libbase::k60::Pin::Name::kPta28
-// #define LIBSC_LINEAR_CCD1_CLK libbase::k60::Pin::Name::kPtb2
-// #define LIBSC_LINEAR_CCD1_AD libbase::k60::Pin::Name::kPtb3
 
 #define LIBSC_OV77250_VSYNC libbase::k60::Pin::Name::kPtc2
 #define LIBSC_OV77250_PCLK libbase::k60::Pin::Name::kPtb6
